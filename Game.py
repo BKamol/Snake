b9ff7a6a7748c2ab4c901920aa4c39e6474b8ead
BKamol <kamoljon2004mm@gmail.com>
2024-08-15 19:49:23 +0300

    Initial commit

---
+++ Game.py
@@ -0,0 +1,205 @@
+import random
+import sys
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor, QPen, QBrush
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QMainWindow, QGraphicsView, QGraphicsScene, QMessageBox
+from game_objects import Snake, Apple, Obstacle, GameObject
+
+
+class Game(QMainWindow):
+    """
+    Окно для отображения поля игры и строки, показывающей очки
+    """
+    WIDTH = 800
+    HEIGHT = 600
+
+    def __init__(self):
+        super().__init__()
+
+        self.statusbar = self.statusBar()
+        self.board = Board(self)
+        
+
+        self.setCentralWidget(self.board)
+        self.setWindowTitle('Snake')
+        self.resize(self.WIDTH, self.HEIGHT)
+        self.center()
+
+        self.show()
+
+    def center(self):
+        """
+        Центрирование окна
+        """
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move(int((screen.width() - size.width()) / 2),
+                  int((screen.height() - size.height()) / 2))
+
+
+class Board(QGraphicsView):
+    """
+    Игровое поле
+    Атрибуты класса:
+    msg2statusbar: pyqtSignal - сигнал, отправляемый статус бару родителя, для отображения очков и состояния игры(пауза, поражение)
+    GameTimer: int - период обновления игры
+    ObstacleTiemr: int - период появления препятствия
+    """
+    msg2statusbar = pyqtSignal(str)
+    GameTimer = 40
+    ObstacleTimer = 10000
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.msg2statusbar.connect(parent.statusbar.showMessage)
+        self.initBoard()
+        self.initGame()
+
+
+    def initBoard(self):
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+        self.setGeometry(0, 0, Game.WIDTH, Game.HEIGHT)
+        self.center()
+        self.setSceneRect(0, 0, self.width(), self.height())
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def initGame(self):
+        self.snake = Snake()
+        self.apple = Apple()
+        self.obstacle = Obstacle()
+        self.game_timer = QTimer()
+        self.obstacle_timer = QTimer()
+        self.paused = False
+
+        self.game_timer.timeout.connect(self.update_game)
+        self.obstacle_timer.timeout.connect(self.generate_obstacle)
+        self.game_timer.start(self.GameTimer)
+        self.obstacle_timer.start(self.ObstacleTimer)
+        self.apple.add_random_item(avoid_object=self.snake)
+        self.msg2statusbar.emit(f"Score: {self.snake.score}")
+
+
+    def update_game(self):
+        """
+        Каждые GameTimer времени обновляет состояние игры
+        """
+        self.draw_objects()
+        self.snake.move()
+        self.check_collisions()
+
+    def draw_objects(self):
+        """
+        Рисует объекты на сцену
+        """
+        self.scene.clear()
+        self._draw_object(self.snake, Qt.green)
+        self._draw_object(self.apple, Qt.red)
+        self._draw_object(self.obstacle, Qt.gray)
+
+    def _draw_object(self, object, color=Qt.black):
+        """
+        Рисует один объект
+        """
+        pen = QPen(Qt.black)
+        brush = QBrush(color)
+        for x, y in object:
+            self.scene.addRect(x*object.CellSize, y*object.CellSize, 
+                               object.CellSize, object.CellSize, pen, brush)
+
+    def check_collisions(self):
+        """
+        Проверяет столкновение змеи и, при обнаружении, меняет состояние игры(больше очков или поражение)
+        """
+        if self.snake.check_self_collision() or self.snake.check_collision(self.obstacle):
+            self.msg2statusbar.emit(f"Game Over. Your score: {self.snake.score}")
+            self.show_popup()
+            self.game_timer.stop()
+            self.obstacle_timer.stop()
+            self.snake.is_alive = False
+
+        if self.snake.check_collision(self.apple, remove=True):
+            self.apple.add_random_item(avoid_object=self.snake)
+            self.snake.move(grow=True)
+            self.msg2statusbar.emit(f"Score: {self.snake.score}")
+
+    def generate_obstacle(self):
+        """
+        Добавляет клетку препятствия
+        """
+        self.obstacle.add_random_item(avoid_object=self.snake)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_P:
+            self.pause()
+            return
+        elif self.paused:
+            return
+
+        if event.key() == Qt.Key_Up:
+            self.snake.direction = 'up'
+        elif event.key() == Qt.Key_Down:
+            self.snake.direction = 'down'
+        elif event.key() == Qt.Key_Left:
+            self.snake.direction = 'left'
+        elif event.key() == Qt.Key_Right:
+            self.snake.direction = 'right'
+        elif event.key() == Qt.Key_R:
+            self.restart()
+
+    def restart(self):
+        """Рестарт игры"""
+        self.initGame()
+
+    def pause(self):
+        """
+        Ставит игру на паузу, если она запущена или запускает, если она на паузе
+        """
+        if self.snake.is_alive:
+            self.paused = not self.paused
+            if self.paused:
+                self.game_timer.stop()
+                self.obstacle_timer.stop()
+                self.msg2statusbar.emit("PAUSE")
+            else:
+                self.game_timer.start(self.GameTimer)
+                self.obstacle_timer.start(self.ObstacleTimer)
+
+    def resizeEvent(self, event):
+        self.setSceneRect(0, 0, self.width(), self.height())
+        self.resize_object_rects()
+        super().resizeEvent(event)
+
+    def resize_object_rects(self):
+        """
+        Обновляет размер игрового поля при обновлении размеров окна
+        """
+        GameObject.BoardHeight = self.height() // GameObject.CellSize
+        GameObject.BoardWidth = self.width() // GameObject.CellSize
+
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move(int((screen.width() - size.width()) / 2),
+                  int((screen.height() - size.height()) / 2))
+
+    def show_popup(self):
+        """
+        Показывает всплывающее окно при поражении
+        """
+        msg = QMessageBox()
+        msg.setWindowTitle("Game Over")
+        msg.setText(f"You have lost. Your score: {self.snake.score}.")
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
+
+    
+
+if __name__ == '__main__':
+    app = QApplication([])
+    snake_game = Game()
+    snake_game.show()
+    sys.exit(app.exec_())
+
---
+++ game_objects.py
@@ -0,0 +1,171 @@
+import random
+
+class GameObject:
+    """
+    Все игровые объекты наследуются от этого класса.
+    Объект представляет из себя список координат [x, y]
+    Атрибуты:
+    _coordinates: list - координаты объекта
+    _size - размер или количество
+
+    Атрибуты класса:
+    CellSize - размер клетки в игровом поле
+    BoardWidth - ширина игрового поля
+    BoardHeight - высота игрового поля
+    """
+    CellSize = 20
+    BoardWidth = 40
+    BoardHeight = 30
+
+    def __init__(self):
+        self._coordinates = []
+        self._size = 0
+    
+    @property
+    def coordinates(self):
+        return self._coordinates
+    
+    @coordinates.setter
+    def coordinates(self, new_coords):
+        self._coordinates = new_coords
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, new_size):
+        self._size = new_size
+
+    def add_random_item(self, avoid_object=None, amount=1):
+        """
+        Добавляет к объекту случайную пару координат.
+        Параметры:
+        avoid_object: GameObject - новая пара не будет появляться на месте координат переданного объекта
+        amount: int - количество добавляемых случайных координат
+        """
+        for _ in range(amount):
+            x = random.randint(1, self.BoardWidth - 1)
+            y = random.randint(1, self.BoardHeight - 1)
+
+            if avoid_object is not None:
+                while [x, y] in avoid_object:
+                    x = random.randint(1, self.BoardWidth - 1)
+                    y = random.randint(1, self.BoardHeight - 1)
+
+            self.size += 1
+            self.coordinates.append([x, y])
+
+    def __getitem__(self, index):
+        return self.coordinates[index]
+    
+    def __setitem__(self, index, value):
+        self.coordinates[index] = value
+
+    def __iter__(self):
+        yield from self.coordinates
+
+
+class Apple(GameObject):
+    pass
+
+
+class Obstacle(GameObject):
+    pass
+
+
+class Snake(GameObject):
+    """
+    Класс для змеи.
+    При инициализации змея размера 3 клетки появляется в случайном месте и движется вниз.
+    Голова змеи находится в конце списка.
+    """
+    def __init__(self):
+        super().__init__()
+        self.size = 3
+        x = random.randint(1, self.BoardWidth-1)
+        y = random.randint(1, self.BoardWidth-1)
+        self.coordinates = [[x, y], [x, y+1], [x, y+1]]
+        self._direction = "down"
+        self.is_alive = True
+
+    @property
+    def score(self):
+        return self.size - 3
+
+    @property
+    def direction(self):
+        return self._direction
+    
+    @direction.setter
+    def direction(self, new_direction):
+        """
+        Меняет направление змеи
+        """
+        if new_direction == 'right' and self._direction != 'left':
+            self._direction = 'right'
+        elif new_direction == 'left' and self._direction != 'right':
+            self._direction = 'left'
+        elif new_direction == 'up' and self._direction != 'down':
+            self._direction = 'up'
+        elif new_direction == 'down' and self._direction != 'up':
+            self._direction = 'down'    
+
+    def move(self, grow=False):
+        """
+        Двигает змею на 1 клекту в направлении _direction
+        Параметры:
+        grow: bool - добавляет к змее 1 клетку размера при значении True
+        """
+        x, y = self.coordinates[-1]
+        if self.direction == 'right':
+            x += 1
+        elif self.direction == 'left':
+            x -= 1
+        elif self.direction == 'up':
+            y -= 1
+        else:
+            y += 1
+
+        if x < 0:
+            x = self.BoardWidth
+        elif x >= self.BoardWidth:
+            x = 0
+
+        if y < 0:
+            y = self.BoardHeight
+        elif y >= self.BoardHeight:
+            y = 0
+
+        if grow:
+            self.size += 1
+        else:
+            self.coordinates.pop(0)
+        self.coordinates.append([x, y])
+
+    def get_head_coords(self):
+        return self.coordinates[-1]
+    
+    def check_collision(self, object, remove=False):
+        """
+        Проверяет столкновение змеи c object
+        Параметры:
+        object: GameObject - объект, проверяемый на столкновение
+        remove: bool - удалет клетку объекта object, которая столкнуласт со змеей при значении True
+        """
+        for pos in object:
+            if pos in self.coordinates:
+                if remove: 
+                    object.coordinates.remove(pos)
+                return True
+        return False
+    
+    def check_self_collision(self):
+        """
+        Проверяет столкновение змеи с собой
+        """
+        for i in range(self.size-1):
+            if self.coordinates[i] == self.coordinates[-1]:
+                return True
+        return False
+
